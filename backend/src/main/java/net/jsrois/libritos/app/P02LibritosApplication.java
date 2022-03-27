56d4a35d77417fb6ef00b5c498223c3962e770a9
Javi <javi@jsrois.net>
2022-03-27 23:48:19 +0200

    Springboot + svelte project with gradle-based configuration

---
+++ backend/src/main/java/net/jsrois/libritos/app/P02LibritosApplication.java
@@ -0,0 +1,13 @@
+package net.jsrois.libritos.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P02LibritosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P02LibritosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/net/jsrois/libritos/app/P02LibritosApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jsrois.libritos.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P02LibritosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
